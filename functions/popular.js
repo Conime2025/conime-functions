e1b34643eab59170f840870dabe964a38f097b3d
Conime2025 <whasyim0202@gmail.com>
2025-06-14 20:13:51 +0700

    Update popular.js

--- functions/popular.js
+++ functions/popular.js
@@ -1,17 +1,10 @@
 exports.handler = async function(event) {
   const { start, end } = event.queryStringParameters || {};
-  const token = process.env.UMAMI_API_KEY;
 
-  console.log("🔐 API KEY dipakai:", token?.slice(0, 10) + "...");
+  const apiUrl = `https://cloud.umami.is/share/uMWnWOBHVuIVhfCZ/monumental-madeleine-1eeaaf.netlify.app/pages?start=${start}&end=${end}`;
 
-  const apiUrl = `https://cloud.umami.is/api/websites/fabe1eb6-5122-4712-b095-5ec2ab778540/stats/pages?start=${start}&end=${end}`;
-  const response = await fetch(apiUrl, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  });
-
-  const text = await response.text(); // ambil raw response
+  const response = await fetch(apiUrl);
+  const text = await response.text();
 
   console.log("📡 Response Status:", response.status);
   console.log("📄 Response Text:", text);
@@ -22,6 +15,6 @@ exports.handler = async function(event) {
       "Access-Control-Allow-Origin": "*",
       "Content-Type": "application/json"
     },
-    body: text // bisa JSON atau error, kita kirim apa adanya dulu
+    body: text
   };
 };
